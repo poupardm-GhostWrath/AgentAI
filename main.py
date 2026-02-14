4521609e5961c0eaa68d8af939dbfceb1a003db1
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-02-13 19:59:15 -0700

    Verbose Output

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ def main():
     
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
     # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    print(f"User prompt: {args.user_prompt}")
+    
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
@@ -30,8 +31,10 @@ def main():
     if response.usage_metadata == None : raise RuntimeError("Failed API request")
 
     # Output from Response
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Response:\n{response.text}")
 
 
