f414a9ce9be228a0bc02ed53a6ed70eec90f380b
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-02-14 10:51:28 -0700

    Write Files Lesson

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,14 +2,21 @@ import os
 from config import CHARACTER_LIMIT
 
 def get_file_content(working_directory, file_path):
+
+    # Set Absolute Path for File and Working Directory
     working_dir_abs = os.path.abspath(working_directory)
     target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
 
+    # Check Path is Valid
     valid_target_file = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
     if valid_target_file == False:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    
+    # Check if target is a file
     if os.path.isfile(target_file) == False:
         return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    # Read the content of the target_file with a CHARACTER_LIMIT
     try:
         file_content = []
         file_object = open(target_file)
--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,20 +1,28 @@
 import os
 
 def get_files_info(working_directory, directory="."):
+
+    # Set Absolute Path for File and Working Directory
     working_dir_abs = os.path.abspath(working_directory)
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
 
-    # Will be True or False
+    # Check Path is Valid
     valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
     if valid_target_dir == False:
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    # Check if Path is a directory
     if os.path.isdir(target_dir) == False:
         return f'Error: "{directory}" is not a directory'
+    
     directory_content = []
+    # Check which directory we are working in for output
     if directory == ".":
         directory_content.append("Result for current directory:")
     else:
         directory_content.append(f"Result for '{directory}' directory:")
+
+    # Read the info and add to the directory_content list
     for item in os.listdir(target_dir):
         try:
             directory_content.append(f"- {item}: file_size={os.path.getsize(target_dir + "/"+ item)} bytes, is_dir={os.path.isdir(target_dir + "/"+ item)}")
