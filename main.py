eeee3aa4dd151505821c8d2c5e23ce3e42962593
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-02-14 11:28:09 -0700

    System Prompt Lesson

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 def main():
     load_dotenv()
@@ -10,6 +11,9 @@ def main():
     # Get API Key
     api_key = os.environ.get("GEMINI_API_KEY")
     if api_key == None : raise RuntimeError("Gemini API Key not found")
+
+    # Get AI Model
+    model_name = os.environ.get("GEMINI_MODEL")
 
     # Create Gemini Client
     client = genai.Client(api_key=api_key)
@@ -27,7 +31,11 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     # Response from Gemini Client
-    response = client.models.generate_content(model='gemini-2.5-flash', contents=messages)
+    response = client.models.generate_content(
+        model=model_name, 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),
+        )
     if response.usage_metadata == None : raise RuntimeError("Failed API request")
 
     # Output from Response
