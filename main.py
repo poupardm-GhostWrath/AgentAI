953a4185dde4a6f3f1ed1992f6d0e5e01d9dedbc
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-02-14 17:46:53 -0700

    Function Declaration Lesson

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv()
@@ -34,7 +35,10 @@ def main():
     response = client.models.generate_content(
         model=model_name, 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0),
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt, 
+            temperature=0),
         )
     if response.usage_metadata == None : raise RuntimeError("Failed API request")
 
@@ -43,7 +47,11 @@ def main():
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(f"Response:\n{response.text}")
+    if response.function_calls != None:
+        for function_call in response.function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
