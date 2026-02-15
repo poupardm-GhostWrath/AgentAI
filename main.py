a3a06b330e10f3201fc0bd8fa3abc62487dd498c
poupardm-GhostWrath <mathpoupard@gmail.com>
2026-02-15 15:33:12 -0700

    Agent Loop Lesson

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from google.genai import types
 from prompts import system_prompt
 from call_function import *
 
+
+def prompt_gemini(client, model_name, messages):
+    return client.models.generate_content(
+        model=model_name, 
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt, 
+            temperature=0),
+        )
+
 def main():
     load_dotenv()
 
@@ -26,44 +37,53 @@ def main():
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
 
-    # prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    
-
+    # Prompt
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    # Response from Gemini Client
-    response = client.models.generate_content(
-        model=model_name, 
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt, 
-            temperature=0),
-        )
-    if response.usage_metadata == None : raise RuntimeError("Failed API request")
+    # Loop
 
-    # Output from Response
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    for i in range(20):
 
-    function_results = []
-    if response.function_calls != None:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, args.verbose)
-            if len(function_call_result.parts) < 1:
-                raise Exception("Error: parts list empty")
-            if function_call_result.parts[0].function_response == None:
-                raise Exception("Error: function response is None")
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception("Error: response is None")
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-            function_results.append(function_call_result.parts[0])
-            #print(f"Calling function: {function_call.name}({function_call.args})")
-    else:
-        print(f"Response:\n{response.text}")
+        # Response from Gemini Client
+        response = prompt_gemini(client, model_name, messages)
+        if response.usage_metadata == None : raise RuntimeError("Failed API request")
+        
+        
+        
+        if response.candidates == None:
+            raise Exception("Error: candidates list empty")
+        
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+        # Output from Response
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        function_results = []
+        if response.function_calls != None:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, args.verbose)
+                if len(function_call_result.parts) < 1:
+                    raise Exception("Error: parts list empty")
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception("Error: function response is None")
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception("Error: response is None")
+                if args.verbose:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")
+                function_results.append(function_call_result.parts[0])
+                #print(f"Calling function: {function_call.name}({function_call.args})"
+            messages.append(types.Content(role="user", parts=function_results))
+        else:
+            print(f"Final response:\n{response.text}")
+            break
+
+        if i == 19:
+            print(f"Error: Max iterations has been reached")
+            exit(1)
     
 
 
--- prompts.py
+++ prompts.py
@@ -9,4 +9,7 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+
+For the final response:
+State what you did. And make the response in a bullet point format to make it easier for the user to understand.
 """
